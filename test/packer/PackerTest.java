a664845f931c298e37613ecf243583da31c26ee4
Ryan Parry <91030867@mail.computerpower.ac.nz>
2019-07-11 17:09:06 +1200

    Extra assertEquals added to be sure to be sure

--- test/packer/PackerTest.java
+++ test/packer/PackerTest.java
@@ -54,6 +54,10 @@ public class PackerTest {
     Manifest m3 = new Manifest();
     Manifest m4 = new Manifest();
     
+    Customer c1 = new Customer("Jim Jones", testAddress0);
+    Customer c2 = new Customer("Tim Tucker", testAddress1);
+    Customer c3 = new Customer("Suzy Smith", testAddress2);
+    Customer c4 = new Customer("Fred Fritz", testAddress3);
 
 
     /**
@@ -62,13 +66,58 @@ public class PackerTest {
     @Test
     public void testPackProducts() {
         m1.addProduct(a1);
-        Customer c1 = new Customer("J", testAddress10);
+        
         Box b1 = new Box(c1, testDepot0);
         b1.addProduct(a1);
         List<Box> bL = new ArrayList<>();
         bL.add(b1);
+        
         assertEquals(b1.toString(), Packer.packProducts(c1, testDepot0, m1).get(0).toString());
-        //System.out.println(Packer.packProducts(c1, testDepot0, m1));
+        
+        m2.addProduct(a1, 2);
+        m2.addProduct(a3);
+        m2.addProduct(a4, 25);
+        assertEquals("Tim Tucker\n1 First St\nAplace\nCitadel City\nA111\n" +
+                "Alpha1 x 2\nAlpha4 x 5\nFRAGILE\nHAZARD\nHEAVY",
+                Packer.packProducts(c2, testDepot0, m2).get(0).toString());
+        //packProducts() emptys the manifest so products need to be added again
+        m2.addProduct(a1, 2);
+        m2.addProduct(a3);
+        m2.addProduct(a4, 24);
+        assertEquals("Tim Tucker\n1 First St\nAplace\nCitadel City\nA111\n" +
+                "Alpha4 x 5\nHAZARD\nHEAVY",
+                Packer.packProducts(c2, testDepot0, m2).get(1).toString());
+        m2.addProduct(a1, 2);
+        m2.addProduct(a3);
+        m2.addProduct(a4, 24);
+        assertEquals("Tim Tucker\n1 First St\nAplace\nCitadel City\nA111\n" +
+                "Alpha4 x 5\nHAZARD\nHEAVY", 
+                Packer.packProducts(c2, testDepot0, m2).get(2).toString());
+        m2.addProduct(a1, 2);
+        m2.addProduct(a3);
+        m2.addProduct(a4, 24);
+        assertEquals("Tim Tucker\n1 First St\nAplace\nCitadel City\nA111\n" +
+                "Alpha3 x 1\nAlpha4 x 4\nFRAGILE\nHAZARD\nHEAVY", 
+                Packer.packProducts(c2, testDepot0, m2).get(4).toString());
+        
+        
+        
+        m3.addProduct(a1, 10);
+        m3.addProduct(a2, 12);
+        m3.addProduct(a4, 6);
+        assertEquals("Suzy Smith\n123 Count St\nBrooklyn\nWelling Town\nB222\n" +
+                "Alpha1 x 10\nAlpha4 x 5\nFRAGILE\nHAZARD\nHEAVY", 
+                Packer.packProducts(c3, testDepot0, m3).get(0).toString());
+        m3.addProduct(a1, 10);
+        m3.addProduct(a2, 12);
+        m3.addProduct(a4, 6);
+        assertEquals("Suzy Smith\n123 Count St\nBrooklyn\nWelling Town\nB222\n" +
+                "Alpha2 x 8\nAlpha4 x 1\nFRAGILE\nHAZARD\nHEAVY", 
+                Packer.packProducts(c3, testDepot0, m3).get(1).toString());
+        m3.addProduct(a1, 10);
+        m3.addProduct(a2, 12);
+        m3.addProduct(a4, 6);
+        System.out.print(Packer.packProducts(c3, testDepot0, m3).get(1).toString());
     }
     
 }
